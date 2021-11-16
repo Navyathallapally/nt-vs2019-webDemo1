a581ebf34b79f173a39356672b6a350d54bf159b
Pavani <pavanigottipati539@gmail.com>
2021-11-16 16:53:16 +0530

    hai navya added

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace nt_vs2019_webDemo1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!\n");
+                    await context.Response.WriteAsync("hai navya");
                 });
             });
         }
